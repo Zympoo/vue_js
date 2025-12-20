840f4cb8dbc07f0e6f3490ee876e7a324adbcef7
Zympoo <warre.neirinck@gmail.com>
2025-12-21 00:33:23 +0100

    oefening 7

---
+++ oefenbundel/bundel/meldingen/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+import Notifications from './components/Notifications.vue';
+
+import { useNotificationsStore } from './stores/notificationStore'
+const notificationsStore = useNotificationsStore()
+
+const notificationMessage = ref("")
+</script>
+<template>
+  <main class="p-6 space-y-8">
+    <div class="w-96 bg-white rounded-lg shadow border p-4 space-y-3">
+    <label class="block text-sm font-medium text-gray-700">
+      Nieuwe melding
+    </label>
+
+    <input
+      type="text" v-model="notificationMessage"
+      placeholder="Typ een melding..."
+      class="w-full px-3 py-2 border rounded focus:outline-none focus:ring-1 focus:ring-blue-400"
+    />
+
+    <button
+      class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 cursor-pointer" @click="notificationsStore.addNotification(notificationMessage)"
+    >
+      Toon melding
+    </button>
+  </div>
+    
+    <div class="fixed top-4 left-1/2 -translate-x-1/2 w-96 space-y-2">
+
+      <!-- Notification -->
+      <Notifications />
+
+    </div>
+  </main>
+</template>
---
+++ oefenbundel/bundel/meldingen/src/components/Notifications.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { useNotificationsStore } from '../stores/notificationStore'
+    import { storeToRefs } from 'pinia'
+
+    const notificationsStore = useNotificationsStore()
+    const { notifications } = storeToRefs(notificationsStore)
+</script>
+<template>
+    <div v-for="notification in notifications" :key="notification.id" class="flex items-center justify-between bg-white border shadow rounded px-4 py-3">
+        <span class="text-gray-800">
+          {{ notification.msg }}
+        </span>
+
+        <button class="text-gray-400 hover:text-gray-600 cursor-pointer" @click="notificationsStore.closeNotification(notification.id)">
+          ✕
+        </button>
+    </div>
+</template>
